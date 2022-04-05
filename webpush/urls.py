334888150318910a47443de647bc383b109249f1
Sny <sny.aggarwal@gmail.com>
2022-04-05 12:10:57 +0530

    using re_path instead of url | django4 update

--- webpush/urls.py
+++ webpush/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import url
+from django.urls import re_path
 
 from . import views
 
 urlpatterns = [
-    url(r'^save_information', views.save_info, name='save_webpush_info'),
+    re_path(r'^save_information', views.save_info, name='save_webpush_info'),
     # Service worker need to be loaded from same domain
-    url(r'^service-worker.js', views.ServiceWorkerView.as_view(), name='service_worker')
+    re_path(r'^service-worker.js', views.ServiceWorkerView.as_view(), name='service_worker')
 ]
